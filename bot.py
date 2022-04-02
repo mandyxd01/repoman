c083b00c283e0d5a8f92930e1da65a8b70d0968a
mandyxd01 <98300527+mandyxd01@users.noreply.github.com>
2022-04-01 21:07:11 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,13 +22,19 @@ footer = ''' '''
 indchats = [-1001675910285]
 indsend_to = -1001663052159
 
+chat1 = [-1001725626602]
+tochat = -1001552017930
 
 fzz = -1001706261502
-serD = [-1001442038816, -1001477756331, -1001782270836, -1001250582595]
+serD = [-1001442038816, -1001477756331]
+
+fez2 = [-1001666837747]
+sed2 = [-1001782270836, -1001250582595]
 
 black = ["Enjoy it ❤❤❤","♨️ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙺𝙰𝚁𝙽𝙴 𝙽𝙰𝙷𝙸 𝙰𝙰𝚁𝙰 𝚃𝙾𝙷 𝙿𝙸𝙽𝙽𝙴𝙳 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝙲𝙷𝙴𝙲𝙺 𝙺𝙰𝚁𝙾","👉 🅱🅰🅲🅺🆄🅿  🅲🅷🅰🅽🅽🅴🅻","▬" ,"➖" ,"=" ,"●╾⁃⁃╼╾⁃⁃╼╾⁃⁃╼╾⁃⁃╼╾⁃⁃╼╾⁃⁃╼╾⁃⁃╼●" ,"🔥Backup file🔥" ,"🔥Join channel 🔥" ,"JOIN CHANNEL 👇" ,"Join adult network🍌💦" ,"SHARE OUR CHANNEL👇" ,"𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗰𝗵𝗮𝗻𝗻𝗲𝗹" ,"𝐉𝐎𝐈𝐍 𝐔𝐒 ➪" ,"🙆‍♀ Join Our Backup:- ","Join now best channel" ,"♨️ SEARCH & JOIN NOW👇","☆☆☆••••••••••••••••☆☆☆","➥" ,]
 
 
+##################### foward 1 ###################
 @client.on(events.NewMessage(chats=indchats))
 async def hello(event):
     # chat = await event.get_chat()
@@ -78,11 +84,74 @@ async def hello(event):
             await client.send_message(indsend_to , caption)
 
 
+            
+            
+######### foewad 2 ######
+@client.on(events.NewMessage(chats=chat1))
+async def hello(event):
+    # chat = await event.get_chat()
+    caption = event.message.message
+    urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
+    if(urls_to_change):
+        try:
+            media = await client.download_media(event.message)
+        except:
+            print("no media")
+        #  this is for blacklist word        
+        for i in black:
+            caption = re.sub(i, "" , caption)
+
+
+        caption = re.sub("@.*" , "" , caption)
+        caption = re.sub("https://t.me/.*" , "" , caption)
+        caption = re.sub("t.me/.*" , "" , caption)
+
+        regrex_pattern = re.compile(pattern = "["
+                    u"\U0001F600-\U0001F64F"  # emoticons
+                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags = re.UNICODE)
+
+        # url to change 
+        for i in urls_to_change:
+            link = regrex_pattern.sub(r'' , i)
+            # print(link)
+            url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+            param = {
+                'token': mdisk_api,
+                'link':link
+                }
+            res = requests.post(url, json = param)
+            shareLink = res.json()['sharelink']
+            # print("changed link : " , shareLink)
+            caption = re.sub(link , shareLink , caption)
+            # print(caption)
+            sleep(0.2)
+        caption = caption + "\n" + footer
+        if media:
+            await client.send_file(tochat ,file=media , caption=caption)
+            os.remove(media)
+        else:
+            await client.send_message(tochat , caption)
 
 
 @client.on(events.NewMessage(chats=fzz))
 async def _(event):
     for i in serD:
+        try:
+            await client.send_message(
+                i,
+                event.message
+            )
+        except Exception as e:
+            print(e)
+            
+            
+            
+@client.on(events.NewMessage(chats=fez2))
+async def _(event):
+    for i in sed2:
         try:
             await client.send_message(
                 i,
