968cb96d2c101775d80ce155cfe93c0460116794
mandyxd01 <98300527+mandyxd01@users.noreply.github.com>
2022-04-08 08:00:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,7 +92,7 @@ async def hello(event):
 ############### mix ##########
 
 @client.on(events.NewMessage(chats=mix1))
-async def hello(event):
+async def hello2(event):
     # chat = await event.get_chat()
     caption = event.message.message
     urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
@@ -142,49 +142,60 @@ async def hello(event):
             await client.send_message(mix1_to , caption)
             
             
-####### for all #######
+####### for web eng #######
 
-chats = [-1001663052159]
-chats_to_send = [-1001696893100, -1001782270836,-1001477756331,-1001142224290,-1001198221154]
+we = [-1001671942243,-1001607066545]
+twe = -1001112334078
 
-messages_toSend = []
-messages_sent = []
-@client.on(events.NewMessage(pattern=r'\.for',chats = chats))
-async def runbollyhandler(event):
-    replied_msg = await event.get_reply_message()
-    
-    chat = await event.get_chat()
-    
-    msg_id = replied_msg.id
-    await client.delete_messages(chat , event.message)
-    
-    
-    
-    while 1:
-        for ch in chats_to_send:
-            try:
-                allmsg = await client.get_messages(chat , None , reverse=True , min_id=(msg_id-1) , max_id=(msg_id+10))
-            except:
-                await client.send_message(720212064 , "Not enogh messages")
-                return 0
-            if len(allmsg) == 0:
-                await client.send_message('@m3nd7' , "Not enogh messages")
-                return 0
-            messages_toSend = []
-            messages_sent = []
-            for msg in allmsg:
-                    messages_toSend.append(msg)
-                    #print(msg)
-            while len(messages_sent) != len(messages_toSend):
-                ran = randint(0 , len(messages_toSend) -1)
-                if not ran in messages_sent:
-                    await client.send_message(ch, messages_toSend[ran])
-                    messages_sent.append(ran)
-                
-        msg_id = msg_id+10
-        print("sleeping")
-        sleep(3600)
-        print("wake")
+@client.on(events.NewMessage(chats=we))
+async def hello1(event):
+    # chat = await event.get_chat()
+    caption = event.message.message
+    urls_to_change = re.findall('https?://mdisk.me/convertor/.*' , caption)
+    if(urls_to_change):
+        try:
+            media = await client.download_media(event.message)
+        except:
+            print("no media")
+        #  this is for blacklist word
+        
+        
+        for i in black:
+            caption = re.sub(i, "" , caption)
+
+
+        caption = re.sub("@.*" , "" , caption)
+        caption = re.sub("https://t.me/.*" , "" , caption)
+        caption = re.sub("t.me/.*" , "" , caption)
+
+        regrex_pattern = re.compile(pattern = "["
+                    u"\U0001F600-\U0001F64F"  # emoticons
+                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags = re.UNICODE)
+
+        # url to change 
+        for i in urls_to_change:
+            link = regrex_pattern.sub(r'' , i)
+            # print(link)
+            url  = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+            param = {
+                'token': mdisk_api,
+                'link':link
+                }
+            res = requests.post(url, json = param)
+            shareLink = res.json()['sharelink']
+            # print("changed link : " , shareLink)
+            caption = re.sub(link , shareLink , caption)
+            # print(caption)
+            sleep(0.2)
+        caption = caption + "\n" + footer
+        if media:
+            await client.send_file(twe ,file=media , caption=caption)
+            os.remove(media)
+        else:
+            await client.send_message(twe , caption)
             
             
           
